58a69484ed2eabca70b9907e20a0b9a04a1f8ca6
Zulfiyya Huseynova <tu7kswf4d@code.edu.az>
2022-05-04 22:08:36 +0400

    task added

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function openFunc(){
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("openbtn").style.visibility="hidden";
+}
+function closeFunc(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("openbtn").style.visibility="visible";
+}
